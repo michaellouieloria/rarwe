1afacb36becc70c4874390db46b6a5df25568358
Michael Louie Loria <michaellouieloria@gmail.com>
2018-02-07 15:18:50 -0600

    sorting and searching with query parameters

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,10 +1,33 @@
 import Controller from '@ember/controller';
 import { isEmpty } from '@ember/utils';
 import { computed } from '@ember/object';
+import { sort } from '@ember/object/computed';
 
 export default Controller.extend({
+  queryParams: {
+    sortBy: 'sort',
+    searchTerm: 's',
+  },
   title: '',
   songCreationStarted: false,
+  sortBy: 'ratingDesc',
+  sortProperties: computed('sortBy', function() {
+    var options = {
+      'ratingDesc': 'rating:desc,title:asc',
+      'ratingAsc': 'rating:asc,title:asc',
+      'titleDesc': 'title:desc',
+      'titleAsc': 'title:asc',
+    };
+    return options[this.get('sortBy')].split(',');
+  }),
+  searchTerm: '',
+  matchingSongs: computed('model.songs.@each.title', 'searchTerm', function() {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model.songs').filter(function(song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+  }),
+  sortedSongs: sort('matchingSongs', 'sortProperties'),
   isAddButtonDisabled: computed('title', function() {
     return isEmpty(this.get('title'));
   }),
@@ -12,6 +35,9 @@ export default Controller.extend({
     return this.get('songCreationStarted') || this.get('model.songs.length');
   }),
   actions: {
+    setSorting: function(option) {
+      this.set('sortBy', option);
+    },
     enableSongCreation: function() {
       this.set('songCreationStarted', true);
     },
